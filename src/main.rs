9159057658cdbfaec7c4c4945d15060e9d70612f
rostegg <rostik.nikolaenko@gmail.com>
2019-11-14 19:37:44 +0200

    ㊗

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 extern crate dragnit;
 extern crate clap; 
+extern crate crimthy_dragnit_schemes ;
+
 use dragnit::*;
 use clap::{Arg, App};
-use std::fs::metadata;
 use std::io;
-use std::fs::File;
 use std::io::prelude::*;
+use crimthy_dragnit_schemes::build_schemes;
+use std::path::{Path, PathBuf};
 
-const SCHEMA_EXTENSION: &str = ".drgn";
 const BINARY_SCHEMA_EXTENSION: &str = ".bdrgn";
-const DEFAULT_BINARY_OUTPUT: &str = "output.bdrgn";
+const DEFAULT_BINARY_OUTPUT: &str = "compiled_schemes";
 
 macro_rules! print_exit_message {
     ($fmt:expr) => (write!(io::stderr(), concat!("error: ", $fmt, "\n")).unwrap());
@@ -22,10 +23,6 @@ macro_rules! error_exit {
         print_exit_message!($($arg)*);
         std::process::exit(1);
     } }
-}
-
-fn is_schema(name: &str) -> bool {
-    return if name.ends_with(SCHEMA_EXTENSION) { true } else { false };
 }
 
 fn is_binary_schema(name: &str) -> bool {
@@ -58,23 +55,48 @@ impl<T> UnwrapErrorHandling<T> for Option<T> {
     }
 }
 
-fn compile_schema(schema_path: &str, target: &str) -> Result<(), io::Error> {
-    println!("{}",schema_path);
-    println!("{}",target);
-    let md = metadata(schema_path)?;    
+fn display_schema(schema_path: &str) -> Result<(), io::Error> {
+    let path = Path::new(schema_path);
+    let file_stem = path.file_stem();
+    // really bad, but really don't care,
+    let schema = Schema::decode(file_stem.unwrap().to_str().unwrap().to_owned(), &std::fs::read(path).unwrap()).unwrap();
+    println!("{:?}", schema);
     Ok(())
 }
 
-fn write_to_file(target_path: &str) -> Result<(), io::Error> {
-    let mut file = r#try!(File::create(target_path));
-    match file.write_all(b"This is a list of my best friends.") {
+fn compile_schema(schema: &Schema, target_path: PathBuf) -> Result<(), io::Error> {
+    let path = target_path.into_os_string().into_string().unwrap();
+    match Schema::save_to(path, schema.encode()) {
         Ok(v) => v,
-        Err(e) => return Err(From::from(e)),
+        Err(e) => println!("{:?}", e),
     }
     Ok(())
 }
 
-fn main_old() {
+fn compile_schemes(schemes_path: &str) -> Result<(), io::Error>{
+    fn make_dir(path: &str) -> std::io::Result<()> {
+        std::fs::create_dir_all(path)?;
+        Ok(())
+    }
+
+    match make_dir(schemes_path) {
+        Ok(v) => v,
+        Err(e) => println!("{:?}", e),
+    }
+
+    let schemes = build_schemes();
+
+    schemes.iter().for_each(|schema| {
+        let path = Path::new(schemes_path).join(format!("{}{}", schema.name, BINARY_SCHEMA_EXTENSION));
+        match compile_schema(schema, path) {
+            Ok(v) => v,
+            Err(e) => println!("{:?}", e),
+        }
+    });
+    Ok(())
+}
+
+fn main() {
     let matches = App::new("dragnit-cli")
                             .version("0.0.1")
                             .author("rostegg")
@@ -83,54 +105,28 @@ fn main_old() {
                                 .short("c")
                                 .long("compile")
                                 .takes_value(true)
-                                .help("Compile single schema or schemas in folder"))
-                            .arg(Arg::with_name("schema-path")
-                                .help("Single schema or schemes folder path")
-                                .required(true)
-                                .index(1))
+                                .help("Compile all schemes to binary"))
+                            .arg(Arg::with_name("display-schema")
+                                .help("Decode single schema")
+                                .short("d")
+                                .long("display-schema")
+                                .takes_value(true))
                             .get_matches();
-
-    let target_schema = matches.value_of("schema-path")
-                        .unwrap_or_exit("Can't unwrap")
-                        .expect_or_exit(&is_schema, "Wrong schema extension (require .drgn)")
-                        .to_owned();
     
     if matches.is_present("compile") {
-        let target_output = matches.value_of("compile")
-                            .unwrap_or(DEFAULT_BINARY_OUTPUT)
-                            .expect_or_exit(&is_binary_schema, "Wrong binary schema extension (require .bdrgn)")
-                            .to_owned();
-                
-        match compile_schema(&*target_schema, &*target_output) {
+        let output_dir = matches.value_of("compile").unwrap_or(DEFAULT_BINARY_OUTPUT);
+        match compile_schemes(output_dir) {
+            Ok(v) => v,
+            Err(e) => println!("{:?}", e),
+        }                       
+    }
+    else if matches.is_present("display-schema") {
+        let target_schema = matches.value_of("display-schema")
+                            .unwrap_or_exit("Can't unwrap target schema path")
+                            .expect_or_exit(&is_binary_schema, "Wrong schema format (.bdrgn required)");
+        match display_schema(&target_schema) {
             Ok(v) => v,
             Err(e) => println!("{:?}", e),
         }
     }
-}
-
-pub fn save_to(target: String, bytes: Vec<u8>) -> std::io::Result<()> {
-    let mut pos = 0;
-    let mut buffer = File::create(target)?;
-    while pos < bytes.len() {
-        let bytes_written = buffer.write(&bytes[pos..])?;
-        pos += bytes_written;
-    }
-    Ok(())
-}
-
-fn main() {
-    let schema = Schema::new(vec![
-        Def::new("Point".to_owned(), DefKind::Struct, vec![
-          Field {name: "x".to_owned(), type_id: TYPE_FLOAT, is_array: false, value: 0},
-          Field {name: "y".to_owned(), type_id: TYPE_FLOAT, is_array: false, value: 0},
-        ]),
-    ]);
-    
-    match save_to("test.output".to_owned(), schema.encode()){
-        Ok(v) => v,
-        Err(e) => println!("{:?}", e),
-    }
-    
-    let schema = Schema::decode(&std::fs::read("test.output").unwrap()).unwrap();
-    println!("{:?}", schema);
 }
