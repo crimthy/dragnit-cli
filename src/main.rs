9c2861f4f376f8f4645c2bd2faa57519dcbf52cb
rostegg <rostik.nikolaenko@gmail.com>
2019-11-14 13:20:45 +0200

    🚃

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 extern crate dragnit;
+extern crate clap; 
 use dragnit::*;
+use clap::{Arg, App, SubCommand};
 
 fn main() {
-    let schema = Schema::new(vec![
-        Def::new("Point".to_owned(), DefKind::Struct, vec![
-          Field {name: "x".to_owned(), type_id: TYPE_FLOAT, is_array: false, value: 0},
-          Field {name: "y".to_owned(), type_id: TYPE_FLOAT, is_array: false, value: 0},
-        ]),
-      ]);
-
-    let value = Value::decode(&schema, 0, &[126, 0, 0, 0, 126, 1, 0, 0]).unwrap();
-
-    println!("{:?}",value);
+    let matches = App::new("dragnit-cli")
+                          .version("0.0.1")
+                          .author("rostegg")
+                          .about("dragnit schema manipulation from console")
+                          .arg(Arg::with_name("config")
+                               .short("c")
+                               .long("config")
+                               .value_name("FILE")
+                               .help("Sets a custom config file")
+                               .takes_value(true))
+                          .arg(Arg::with_name("INPUT")
+                               .help("Sets the input file to use")
+                               .required(true)
+                               .index(1))
+                          .arg(Arg::with_name("v")
+                               .short("v")
+                               .multiple(true)
+                               .help("Sets the level of verbosity"))
+                          .subcommand(SubCommand::with_name("test")
+                                      .about("controls testing features")
+                                      .version("1.3")
+                                      .author("Someone E. <someone_else@other.com>")
+                                      .arg(Arg::with_name("debug")
+                                          .short("d")
+                                          .help("print debug information verbosely")))
+                          .get_matches();
+    println!("{:?}", matches)
 }
-
